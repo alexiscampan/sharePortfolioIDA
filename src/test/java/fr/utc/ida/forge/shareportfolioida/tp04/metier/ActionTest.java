5257564743cc03411491bc080f825f7f9a5a7e67
alexiscampan <alexis.campan@ut-capitole.fr>
2023-01-17 12:10:04 +0100

    [NEW] sonar link added

---
+++ src/test/java/fr/utc/ida/forge/shareportfolioida/tp04/metier/ActionTest.java
@@ -0,0 +1,15 @@
+package fr.utc.ida.forge.shareportfolioida.tp04.metier;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public class ActionTest extends TestCase{
+    @Test
+    public void testEquals() {
+        assertEquals(1,1);
+    }
+}
